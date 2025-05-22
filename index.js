ca059d95dd61129aa203f69bf0a11ee8c533b3f8
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-05-22 09:56:21 -0300

    Criado a rota /logs/:id com ID e mensagem de sucesso gerados

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const fs = require('fs');
+const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
-const express = require('express'); 
 
-const app = express();
-const PORT = 3000;
+function adicionarLog(nomeAluno){
+    const idUnico = uuidv4();
+    const dataHora = new Date().toLocaleString("pt-BR");
+    const mensagem = `${idUnico}, ${dataHora}, ${nomeAluno}/n`;
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Servidor Express configurado com sucesso!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
-
-function adicionarLog(nomeAluno) {
-  const idUnico = uuidv4();
-  const dataHora = new Date().toLocaleString('pt-BR');
-  const mensagem = `${idUnico}, ${dataHora}, ${nomeAluno}\n`;
-
-  fs.appendFile('logs.txt', mensagem, 'utf8', (err) => {
-    if (err) {
-      console.error('Erro ao adicionar mensagem ao arquivo:', err);
-    }
-  });
+    fs.appendFile('logs.txt', mensagem, 'utf8', (err) => {
+        if (err){
+            console.error('Erro para escrever no arquivo:', err);
+        }else{
+            console.log('Adicionado com sucesso');
+        }
+    });
 }
+
+adicionarLog("Beatriz Becker Feldman")
