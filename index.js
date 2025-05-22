a6ca770f1d48035de9fdb80ca9cea56a288a360e
BEATRIZ BECKER FELDMAN <beatriz_feldman@estudante.sesisenai.org.br>
2025-05-22 09:29:42 -0300

    adicionado o modulo uuid

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 //arquivo logs.txt
 fs.writeFileSync('logs.txt', 'Log inicial\n', (err) => {
@@ -6,6 +7,11 @@ fs.writeFileSync('logs.txt', 'Log inicial\n', (err) => {
   console.log('Arquivo logs.txt criado!');
 });
 
+function adicionarLog(nomeAluno){
+  const idUnico = uuidv4();
+  const dataHora = new Date().toLocaleString("pt-BR");
   const mensagem = `${idUnico}, ${dataHora}, ${nomeAluno}/n`;
-fs.appendFile('logs.txt', mensagem, 'utf8', (err) => 
+  
+  fs.appendFile('logs.txt', mensagem, 'utf8', (err) => {
 });
+}
